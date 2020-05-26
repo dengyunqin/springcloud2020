47ff9510e8b569b5b81549d777e9cd73fafcaddb
dengyunqing <1046660219@qq.com>
2020-05-26 17:21:55 +0800

    add payment entitie

---
+++ cloud-provider-payment8001/src/main/java/com/deng/entities/CommonResult.java
@@ -0,0 +1,8 @@
+package com.deng.entities;
+
+/**
+ * Created by dengyunqing on 2020/5/22.
+ */
+public class CommonResult {
+
+}
--- cloud-provider-payment8001/src/main/java/com/deng/entities/Payment.java
+++ cloud-provider-payment8001/src/main/java/com/deng/entities/Payment.java
@@ -1,7 +1,12 @@
 package com.deng.entities;
 
+import lombok.Data;
+
 /**
  * Created by dengyunqing on 2020/5/22.
  */
+@Data
 public class Payment {
+    private long id;
+    private String serial;
 }
